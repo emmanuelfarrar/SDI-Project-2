cd4f3f2c9e8af14e081ce758e08b9ae8daf8b103
emmanuelfarrar <emmanuel.farrar@fullsail.edu>
2014-06-12 23:35:13 +0430

    Change 4
    Created number and string function(tested & working)

--- farrar-emmanuel-assignment2.js
+++ farrar-emmanuel-assignment2.js
@@ -15,6 +15,7 @@ var numberReturn;
 var stringReturn; 
 
 
+
 //FUNCTIONS
 
 //Procedure using value given from variable myPrompt
@@ -33,33 +34,53 @@ var procedureFunction = function(myPrompt)
 //Boolean Function: 
 var booleanFunction = function (myConfirm,food)
 {
-var food;
-while (myConfirm === true)
+	var food;    //variable to identify type of food
+//while loop to compare food type from function call
+	while (myConfirm === true)
 {
-	if (food === "Buttons in a Blanket")
-	{
-	console.log("The best food buff for DPS is " + food + ".");
-	break;
-	}else{
-	console.log("The best food buff for Tanks and Healers is " + food +".");
-	break;
-	}
+		if (food === "Buttons in a Blanket")
+		{
+		console.log("The best food buff for DPS is " + food + ".");
+		break;
+		}else{
+		console.log("The best food buff for Tanks and Healers is " + food +".");
+		break;
+		}
 }
 	return food;
 };
 
 //Number Function: 
-var numberFunction = function ()
+var numberFunction = function (numVar)
 {
-
+var numFood;
+//need to find a use to loop this
+var loopCounter;
+	for (loopCounter = 1; loopCounter <= 4; loopCounter++)
+	{
+	numFood = 5 * numVar
+	console.log("You will need this much food " + numFood);	
+	}
+	return numFood;
 };
 
+//String Function
+var stringFunction = function () 
+{
+var playerRole = "DPS";
+var playerJob = "Bard";
+
+console.log("The role we need to fill is that of a " + playerRole +" playing as a " +playerJob +".");
+return playerRole;
+return playerJob;
+}
+
 //MAIN CODE
-//myConfirm = confirm("CONFIRM SOMETHING");
-//function call
 procedureFunction(myPrompt);
 myConfirm = confirm("CONFIRM SOMETHING");
 booleanReturn = booleanFunction(myConfirm, "Buttons in a Blanket");
+numberReturn = numberFunction(2);
+stringFunction();
 
 
 
